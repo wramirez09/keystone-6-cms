94349e740c474cd926f55d98e141601e495d6a72
will ramirez <wramirez1980@gmail.com>
2021-07-09 13:28:00 -0500

    add Env file

--- keystone.ts
+++ keystone.ts
@@ -7,7 +7,7 @@ import { lists } from './schema';
 let sessionSecret = process.env.SESSION_SECRET;
 
 if (!sessionSecret) {
-  if (process.env.NODE_ENV !== 'production') {
+  if (process.env.NODE_ENV === 'production') {
     throw new Error(
       'The SESSION_SECRET environment variable must be set in production'
     );
